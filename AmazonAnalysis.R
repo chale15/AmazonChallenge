001c784cbb3528cce22f5d330420d39e48ccb16f
Carter Hale <chale15@byu.edu>
2024-10-11 12:10:09 -0600

    Add Penalized_Logistic.R

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(tidymodels)
 library(embed)
 library(vroom)
+library(workflows)
 
 #Read Data
 
@@ -10,18 +11,20 @@ setwd("~/Desktop/Fall 2024/Stat 348/GitHubRepos/amazon-employee-access-challenge
 train <- vroom("train.csv")
 test <- vroom("test.csv")
 
+train <- train %>% mutate(ACTION = as.factor(ACTION))
+
 
 #Visualize Data
 
-vis_recipe <- recipe(ACTION~., data = train) %>% 
-  step_mutate_at(all_numeric_predictors(), fn = factor)
+#vis_recipe <- recipe(ACTION~., data = train) %>% 
+#  step_mutate_at(all_numeric_predictors(), fn = factor)
 
-prep_vis <- prep(vis_recipe)
-factored <- bake(prep_vis, new_data = train)
+#prep_vis <- prep(vis_recipe)
+#factored <- bake(prep_vis, new_data = train)
 
-summary(factored)
+#summary(factored)
 
-corr_plot <- corrplot::corrplot(cor(train))
+#corr_plot <- corrplot::corrplot(cor(train))
 
 #Dummy Variable Encoding
 
@@ -31,5 +34,29 @@ my_recipe <- recipe(ACTION~., data = train) %>%
   step_dummy(all_nominal_predictors())
 
 
-prepped <- prep(my_recipe)
-baked <- bake(prepped, new_data= train)
+#prepped <- prep(my_recipe)
+#baked <- bake(prepped, new_data= train)
+
+#Fit Model
+
+log_reg_model <- logistic_reg() %>% 
+  set_engine('glm')
+
+log_reg_workflow <- workflow() %>% 
+  add_model(log_reg_model) %>% 
+  add_recipe(my_recipe) %>% 
+  fit(data = train)
+
+log_reg_preds <- predict(log_reg_workflow, 
+                         new_data = test,
+                         type = 'prob')
+
+#Format for Submission
+
+log_reg_submission <- log_reg_preds %>% 
+  bind_cols(., test) %>% 
+  select(id, .pred_1) %>% 
+  rename(ACTION = .pred_1) 
+
+vroom_write(x=log_reg_submission, file="./LogRegPreds1.csv", delim=",")
+
---
+++ Penalized_Logistic.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(tidymodels)
+library(embed)
+library(vroom)
+library(workflows)
+library(glmnet)
+
+#Read Data
+
+setwd("~/Desktop/Fall 2024/Stat 348/GitHubRepos/amazon-employee-access-challenge/")
+
+train <- vroom("train.csv")
+test <- vroom("test.csv")
+
+train <- train %>% mutate(ACTION = as.factor(ACTION))
+
+
+#Dummy Variable Encoding
+
+my_recipe <- recipe(ACTION~., data = train) %>% 
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
+  step_normalize(all_numeric_predictors())
+
+
+#Fit Model
+
+plog_model <- logistic_reg(mixture=tune(), penalty = tune()) %>% 
+  set_engine('glmnet')
+
+plog_workflow <- workflow() %>% 
+  add_model(plog_model) %>% 
+  add_recipe(my_recipe)
+
+tuning_grid <- grid_regular(penalty(), mixture(), levels = 5)
+
+folds <- vfold_cv(train, v = 10, repeats=1)
+
+cv_results <- plog_workflow %>% 
+  tune_grid(resamples = folds,
+            grid = tuning_grid, 
+            metrics = metric_set(roc_auc))
+
+best_tune <- cv_results %>% select_best(metric='roc_auc')
+
+final_workflow <- plog_workflow %>% 
+  finalize_workflow(best_tune) %>% 
+  fit(data = train)
+
+plog_preds <- predict(final_workflow, 
+                         new_data = test,
+                         type = 'prob')
+
+#Format for Submission
+
+plog_submission <- plog_preds %>% 
+  bind_cols(., test) %>% 
+  select(id, .pred_1) %>% 
+  rename(ACTION = .pred_1) 
+
+vroom_write(x=plog_submission, file="./Submissions/PLogPreds1.csv", delim=",")
